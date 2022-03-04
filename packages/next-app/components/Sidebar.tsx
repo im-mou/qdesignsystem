75319221265ffc4f57567d59d6a7223340d42e83
Mohsin Riaz <wilddogers@gmail.com>
2022-03-04 03:05:52 +0100

    added more stuff

--- packages/next-app/components/Sidebar.tsx
+++ packages/next-app/components/Sidebar.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import routes from '../constants/routes.json';
 import { useRouter } from 'next/router';
-import Link from 'next/link'
+import Link from 'next/link';
 import { makeStyles } from '@material-ui/core/styles';
 import List from '@material-ui/core/List';
 import ListItem from '@material-ui/core/ListItem';
